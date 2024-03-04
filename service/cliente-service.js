1fff9aaf2eecebe68504180005a59b0b8339f515
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-03-04 12:57:19 -0300

    refactor: callback hell

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -1,10 +1,44 @@
-const http = new XMLHttpRequest();
+const createNewArrow = (nome, email) => {
+    const arrowNewClient = document.createElement('tr');
+    const content = `
+        <td class="td" data-td>${nome}</td>
+        <td>${email}</td>
+        <td>
+            <ul class="tabela__botoes-controle">
+                <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+                <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+            </ul>
+        </td>
+    `;
 
-http.open('GET', 'http://localhost:300/profile');
-
-http.send();
-
-http:onload = () => {
-    const data = http.response;
-    console.log(data);
+    arrowNewClient.innerHTML = content;
+    return arrowNewClient;
 };
+
+const table = document.querySelector('[data-tabela]');
+
+
+const listClients = () => {
+    const promise = new Promise((resolve, reject) => {
+        const http = new XMLHttpRequest();
+        http.open('GET', 'http://localhost:3000/profile');
+        
+        http.onload = () => {
+            if(http.status >= 400) {
+                reject(JSON.parse(http.response));
+            } else {
+                resolve(JSON.parse(http.response))
+            }
+        };
+
+        http.send();       
+    });
+    return promise;
+};
+
+listClients()
+.then(data => {
+    data.forEach(element => {
+        table.appendChild(createNewArrow(element.nome, element.email));
+    });
+});
