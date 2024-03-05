c68fb7566db3916621e052e18e04362f5271bb29
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-03-05 07:05:24 -0300

    api fetch separated module

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -1,33 +1,10 @@
-const createNewArrow = (nome, email) => {
-    const arrowNewClient = document.createElement('tr');
-    const content = `
-        <td class="td" data-td>${nome}</td>
-        <td>${email}</td>
-        <td>
-            <ul class="tabela__botoes-controle">
-                <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-                <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-            </ul>
-        </td>
-    `;
-
-    arrowNewClient.innerHTML = content;
-    return arrowNewClient;
-};
-
-const table = document.querySelector('[data-tabela]');
-
-
-const listClients = () => {
+export const listClients = () => {
     return fetch(`http://localhost:3000/profile`)
     .then(answer => {
         return answer.json();
     });
 };
 
-listClients()
-.then(data => {
-    data.forEach(element => {
-        table.appendChild(createNewArrow(element.nome, element.email));
-    });
-});
+export const clienteService = {
+    listClients
+}
