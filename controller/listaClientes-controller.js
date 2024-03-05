eebbdb58edae95620ec37d834fb66a641647dcc9
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-03-05 07:05:09 -0300

    api return separated module

---
+++ controller/listaClientes-controller.js
@@ -0,0 +1,27 @@
+import { clienteService } from "../service/cliente-service.js";
+
+const createNewArrow = (nome, email) => {
+    const arrowNewClient = document.createElement('tr');
+    const content = `
+        <td class="td" data-td>${nome}</td>
+        <td>${email}</td>
+        <td>
+            <ul class="tabela__botoes-controle">
+                <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+                <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+            </ul>
+        </td>
+    `;
+
+    arrowNewClient.innerHTML = content;
+    return arrowNewClient;
+};
+
+const table = document.querySelector('[data-tabela]');
+
+clienteService.listClients()
+.then(data => {
+    data.forEach(element => {
+        table.appendChild(createNewArrow(element.nome, element.email));
+    });
+});
